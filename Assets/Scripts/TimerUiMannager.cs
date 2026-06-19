b8cdcd8886d8c4c3821a31856f31f2aaf0cd1d9d
TINCHITO123 <bs2893@est.ort.edu.ar>
2026-06-19 08:53:38 -0300

    FALFA BASTANTE EFUERZO
    FALFA BASTANTE EFUERZO

---
+++ Assets/Scripts/TimerUiMannager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimerUiMannager : MonoBehaviour
+{
+    public TextMeshProUGUI txtTimer;
+
+    float timeRemaining = 60f;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+
+            int seconds = Mathf.CeilToInt(timeRemaining);
+
+            txtTimer.text = "00:" + seconds.ToString("00");
+        }
+    }
+}
